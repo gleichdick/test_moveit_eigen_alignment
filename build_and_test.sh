acf9abf7f538649a27297011c753682bcbc442fe
Bjar Ne <gleichdick@users.noreply.github.com>
2020-04-15 15:57:11 +0200

    remove rqt_msg

--- build_and_test.sh
+++ build_and_test.sh
@@ -10,11 +10,11 @@ travis_fold start catkin_build "Building workspace"
 
 Xvfb -screen 0 640x480x24 :99 &
 export DISPLAY=:99.0
-travis_run_true glxinfo -B
+travis_run glxinfo -B
 
-travis_run_true catkin config --install --init --no-extend --jobserver --cmake-args $CMAKE_ARGS
+travis_run catkin config --install --init --no-extend --jobserver --cmake-args $CMAKE_ARGS
 
-travis_run_true catkin build
+travis_run catkin build
 
 travis_fold end catkin_build
 
@@ -22,15 +22,15 @@ source install/setup.sh
 
 travis_fold start catkin_build_tests "Building Tests"
 
-travis_run_true catkin build --make-args tests
+travis_run catkin build --make-args tests
 
 travis_fold end catkin_build_tests
 
 
 travis_fold start catkin_run_tests "Run Tests"
 
-travis_run_true catkin build --catkin-make-args run_tests
+travis_run catkin build --catkin-make-args run_tests
 
 travis_fold end catkin_run_tests
 
-travis_run_true catkin_test_results
+travis_run catkin_test_results
--- install_deps.sh
+++ install_deps.sh
@@ -4,30 +4,30 @@ source .moveit_ci/util.sh
 
 travis_fold start apt_build_system "Install build system"
 
-travis_run_true sudo apt-get update
-travis_run_true sudo apt-get install -qq -y build-essential cmake python-pip xvfb mesa-utils
+travis_run sudo apt-get update
+travis_run sudo apt-get install -qq -y build-essential cmake python-pip xvfb mesa-utils
 
-travis_run_true sudo apt-get install -y -qq libccd-dev libqglviewer-dev-qt5
+travis_run sudo apt-get install -y -qq libccd-dev libqglviewer-dev-qt5
 
-travis_run_true sudo pip install -U setuptools
-travis_run_true sudo pip install rosdep wstool rosinstall catkin_tools
+travis_run sudo pip install -U setuptools
+travis_run sudo pip install rosdep wstool rosinstall catkin_tools
 
-travis_run_true sudo rosdep init
-travis_run_true rosdep update
+travis_run sudo rosdep init
+travis_run rosdep update
 
 travis_fold end apt_build_system
 
 travis_fold start run_wstool "Download ROS packages"
 
-travis_run_simple mkdir -p workspace
-travis_run_simple cd workspace
-travis_run_true wstool init -j8 src ../install.rosinstall
-travis_run_simple --assert sed -i s/fcl/libfcl-dev/g src/fcl/package.xml
+mkdir -p workspace
+cd workspace
+travis_run wstool init -j8 src ../install.rosinstall
+travis_run_simple --no-timing sed -i s/fcl/libfcl-dev/g src/fcl/package.xml
 
 travis_fold end run_wstool
 
 travis_fold start run_rosdep "Installing dependencies"
 
-travis_run_true rosdep install --from-paths src --ignore-src --rosdistro melodic -y
+travis_run rosdep install --from-paths src --ignore-src --rosdistro melodic -y
 
 travis_fold end run_rosdep
