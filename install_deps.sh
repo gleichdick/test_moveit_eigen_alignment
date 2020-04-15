d9ed2c10a83ccaaded4cd8f9e2a07714bca5ed13
Bjar Ne <gleichdick@users.noreply.github.com>
2020-04-15 13:53:20 +0200

    add sudo and kdl

--- install_deps.sh
+++ install_deps.sh
@@ -2,18 +2,17 @@
 
 set -e
 
-apt-get update && apt-get dist-upgrade -y
-apt-get install -y build-essential cmake python-pip
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo apt-get install -y build-essential cmake python-pip
 
-pip install -U setuptools
-pip install rosdep wstool rosinstall catkin_tools
+sudo pip install -U setuptools
+sudo pip install rosdep wstool rosinstall catkin_tools
 
-rosdep init
+sudo rosdep init
 rosdep update
 
-mkdir -p workspace && pushd workspace
+mkdir -p workspace && cd workspace
 wstool init -j8 src ../install.rosinstall
 sed -i s/fcl/libfcl-dev/g src/fcl/package.xml
-
 
 rosdep install --from-paths src --ignore-src --rosdistro melodic -y
