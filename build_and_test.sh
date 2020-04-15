130250bf2f3667f7eb8fe23c4a3cec27b2086cbf
Bjar Ne <gleichdick@users.noreply.github.com>
2020-04-15 15:24:58 +0200

    travis folding

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+source .moveit_ci/util.sh
+
 set -e
 
 cd workspace
+
+
+travis_fold start catkin_build "Building workspace"
 
 catkin config --install --init --no-extend --jobserver --cmake-args $CMAKE_ARGS
 
 catkin build
 
+travis_fold end catkin_build
+
+source install/setup.sh
+
+travis_fold start catkin_build_tests "Building Tests"
+
 catkin build --make-args tests
 
+travis_fold end catkin_build_tests
+
+
+travis_fold start catkin_run_tests "Run Tests"
+
 catkin build --catkin-make-args run_tests
+
+travis_fold end catkin_run_tests
 
 catkin_test_results
--- install_deps.sh
+++ install_deps.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y build-essential cmake python-pip
+source .moveit_ci/util.sh
 
-sudo apt-get install libccd-dev libqglviewer-dev-qt5
+travis_fold start apt_build_system "Install build system"
+
+sudo apt-get update
+sudo apt-get install -qq -y build-essential cmake python-pip
+
+sudo apt-get install -y -qq libccd-dev libqglviewer-dev-qt5
 
 sudo pip install -U setuptools
 sudo pip install rosdep wstool rosinstall catkin_tools
@@ -13,8 +17,18 @@ sudo pip install rosdep wstool rosinstall catkin_tools
 sudo rosdep init
 rosdep update
 
+travis_fold end apt_build_system
+
+travis_fold start wstool "Download ROS packages"
+
 mkdir -p workspace && cd workspace
 wstool init -j8 src ../install.rosinstall
 sed -i s/fcl/libfcl-dev/g src/fcl/package.xml
 
+travis_fold end wstool
+
+travis_fold start rosdep "Installing dependencies"
+
 rosdep install --from-paths src --ignore-src --rosdistro melodic -y
+
+travis_fold end rosdep
